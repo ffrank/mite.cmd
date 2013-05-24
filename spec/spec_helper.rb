410de3e12ce9201f9d7bb131a1739af7ee6ec7de
Jakob Skjerning <jakob@mentalized.net>
2013-05-24 10:33:35 +0200

    Get rid of RSpec 2 deprecation warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'mite_cmd'
 
-Spec::Runner.configure do |config|
+RSpec.configure do |config|
 
 end
