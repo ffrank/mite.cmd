dc08afbb4da7aff881aefbd568ff04c4078d18ef
Felix Frank <felix.frank@unbelievable-machine.com>
2016-03-08 15:44:49 +0100

    separate out specific parts of the core application

--- lib/mite_cmd/application.rb
+++ lib/mite_cmd/application.rb
@@ -1,4 +1,5 @@
-require 'mite_cmd/autocomplete/cached.rb'
+require 'mite_cmd/application/autocomplete'
+require 'mite_cmd/application/time_entries'
 require 'optparse'
 
 if RUBY_VERSION >= '1.9'
@@ -9,6 +10,9 @@ end
 
 module MiteCmd
   class Application
+    include MiteCmd::Application::AutoComplete
+    include MiteCmd::Application::TimeEntries
+
     TIME_FORMAT = /^(\d+(\.\d+)?:?\+?)$|(\d+:\d+\+?)$|\+$/
 
     def initialize(arguments=[])
@@ -33,16 +37,6 @@ module MiteCmd
 
     private
 
-    def auto_complete(arguments)
-      autocomplete = MiteCmd::Autocomplete::Cached.new(MiteCmd.calling_script)
-      #autocomplete.completion_table = MiteCmd::CompletionTable.new(cache_file)
-      if MiteCmd.autocomplete_always_quote
-        autocomplete.suggestions.map(&:quote).each { |s| tell s }
-      else
-        autocomplete.suggestions.map(&:quote_if_spaced).each { |s| tell s }
-      end
-    end
-
     def configure(arguments)
       raise MiteCmd::Exception.new('mite configure needs two arguments, the account name and the apikey') if @arguments.size < 3
       MiteCmd.load_configuration
@@ -53,69 +47,6 @@ module MiteCmd
       write_configuration(settings)
 
       tell("Couldn't set up bash completion. I'm terribly frustrated. Maybe 'mite help' helps out.") unless try_to_setup_bash_completion
-    end
-
-    def create_time_entry(arguments)
-      time_entry = prepare_time_entry(arguments)
-      time_entry.save
-    end
-
-    def prepare_time_entry(arguments)
-      attributes = @default_attributes
-      if @date
-        attributes[:at] = @date
-      end
-
-      begin
-        attributes = fill_in_time_entry_attributes!(arguments)
-      rescue MiteCmd::Exception => e
-        help arguments
-        raise e
-      end
-
-      if attributes[:_start_tracker]
-        start_tracker = true
-        attributes.delete :_start_tracker
-      end
-
-      time_entry = Mite::TimeEntry.new attributes
-      time_entry.start_tracker if start_tracker
-      tell time_entry.inspect
-      time_entry
-    end
-
-    def fill_in_time_entry_attributes!(arguments)
-      attributes = {}
-
-      if arguments[0] =~ TIME_FORMAT
-        time_string = arguments[0]
-        attributes[:minutes] = parse_minutes(time_string)
-        if time_string =~ /\+$/
-          attributes[:_start_tracker] = true
-        end
-      else
-        raise MiteCmd::Exception.new "the first argument to 'mite add' must be a time specification"
-      end
-
-      if project = find_or_create_project(arguments[1])
-        attributes[:project_id] = project.id
-      end
-      if @arguments[2] && service = find_or_create_service(arguments[2])
-        attributes[:service_id] = service.id
-      end
-      if note = arguments[3..-1] * " "
-        attributes[:note] = note
-      end
-
-      if !project.id || !service.id || !note
-        raise "missing project/service/note"
-      end
-
-      attributes
-    end
-
-    def current
-      tell Mite::Tracker.current ? Mite::Tracker.current.inspect : "No current entry"
     end
 
     def method_for_command(command)
@@ -161,121 +92,8 @@ available commands:
       EOH
     end
 
-    def note(arguments)
-      if time_entry = Mite::TimeEntry.first(:params => {:at => 'today'})
-        time_entry.note = [time_entry.note, *arguments].compact.join(' ')
-        time_entry.save
-        tell time_entry.inspect
-      end
-    end
-
     def open(arguments)
       open_or_echo Mite.account_url
-    end
-
-    def rebuild_cache(arguments)
-      rebuild_completion_table
-      tell 'The rebuilding of the cache has been done, Master. Your wish is my command.'
-    end
-
-    def report(arguments)
-      if arguments.length > 1
-        raise MiteCmd::Exception.new "The report subcommand takes exactly one argument with a time specification"
-      elsif arguments.length == 1
-        report_date = arguments[0]
-      end
-
-      if @date
-        if report_date
-          tell "warning: ignoring report argument '#{arguments[0]}' in favor of date parameter '#{@date}'".colorize(:yellow)
-        end
-        report_date = @date
-      end
-
-      report_date ||= "today"
-
-      count = 0
-      total_minutes = 0
-      total_revenue = Mite::TimeEntry.all(:params => {:at => report_date, :user_id => 'current'}).each do |time_entry|
-        total_minutes += time_entry.minutes
-        tell "%3d %s" % [ count, time_entry.inspect ]
-        count += 1
-      end.map(&:revenue).compact.sum
-      tell ("     %s:%.2d" % [total_minutes/60, total_minutes-total_minutes/60*60]).colorize(:lightred) + ", " + ("%.2f $" % (total_revenue/100)).colorize(:lightgreen)
-    end
-
-    def destroy_time_entry(arguments)
-      if arguments.length != 1
-        raise MiteCmd::Exception.new "The delete subcommand takes exactly one argument with a numeric index"
-      end
-      destroy_index = arguments[0].to_i
-      destroy_date = @date || 'today'
-
-      entries = Mite::TimeEntry.all(:params => {:at => destroy_date, :user_id => 'current'})
-      tell "%3d %s" % [ destroy_index, entries[destroy_index].inspect ]
-      if @confirmed
-        entries[destroy_index].destroy
-        tell "the above entry has been permanently removed".colorize(:red)
-      else
-        tell "not destroying the specified entry, add --really to confirm".colorize(:yellow)
-      end
-    end
-
-    def start(arguments)
-      if time_entry = Mite::TimeEntry.first(:params => {:at => 'today'})
-        time_entry.start_tracker
-        tell time_entry.inspect
-      else
-        tell "Oh my dear! I tried hard, but I could'nt find any time entry for today."
-      end
-    end
-
-    def stop(arguments)
-      if current_tracker = (Mite::Tracker.current ? Mite::Tracker.current.stop : nil)
-        tell current_tracker.time_entry.inspect
-      end
-    end
-
-    def find_or_create_by_name(repository, name)
-      object = repository.first(:params => {:name => name})
-      return nil if name =~ TIME_FORMAT
-      object ? object : repository.create(:name => name)
-    end
-
-    def find_or_create_project(name)
-      find_or_create_by_name(Mite::Project, name)
-    end
-
-    def find_or_create_service(name)
-      find_or_create_by_name(Mite::Service, name)
-    end
-
-    def parse_note(args, time_string)
-      if args[3]
-        args[3]
-      elsif time_string.nil? && args[2]
-        args[2]
-      elsif time_string && args[args.index(time_string)+1]
-        args[args.index(time_string)+1]
-      else
-        nil
-      end
-    end
-
-    def parse_minutes(string)
-      string = string.sub(/\+$/, '')
-
-      if string.blank?
-        0
-      elsif string =~ /^\d+:\d+$/
-        string.split(':').first.to_i*60 + string.split(':').last.to_i
-      elsif string =~ /^\d+(\.\d+)?:?$/
-        (string.to_f*60).to_i
-      end
-    end
-
-    def rebuild_completion_table
-      MiteCmd::CompletionTable.new(cache_file).rebuild
     end
 
     def open_or_echo(open_argument)
@@ -287,24 +105,6 @@ available commands:
         YAML.dump(config, f)
       end
       File.chmod(0600, File.expand_path('~/.mite.yml'))
-    end
-
-    def try_to_setup_bash_completion
-      bash_code = "\n\n#{MiteCmd::BASH_COMPLETION}"
-
-      ['~/.bash_completion', '~/.bash_profile', '~/.bash_login', '~/.bashrc'].each do |file|
-        bash_config_file = File.expand_path file
-        next unless File.file?(bash_config_file)
-        unless File.read(bash_config_file) =~ /#{bash_code}/
-          File.open(bash_config_file, 'a') do |f|
-            f.puts bash_code
-          end
-          return true
-        else
-          return true
-        end
-      end
-      return false
     end
 
     def option_parser
---
+++ lib/mite_cmd/application/time_entries.rb
@@ -0,0 +1,161 @@
+
+module MiteCmd
+  module Application::TimeEntries
+
+    def create_time_entry(arguments)
+      time_entry = prepare_time_entry(arguments)
+      time_entry.save
+    end
+
+    def prepare_time_entry(arguments)
+      attributes = @default_attributes
+      if @date
+        attributes[:at] = @date
+      end
+
+      begin
+        attributes = fill_in_time_entry_attributes!(arguments)
+      rescue MiteCmd::Exception => e
+        help arguments
+        raise e
+      end
+
+      if attributes[:_start_tracker]
+        start_tracker = true
+        attributes.delete :_start_tracker
+      end
+
+      time_entry = Mite::TimeEntry.new attributes
+      time_entry.start_tracker if start_tracker
+      tell time_entry.inspect
+      time_entry
+    end
+
+    def fill_in_time_entry_attributes!(arguments)
+      attributes = {}
+
+      if arguments[0] =~ TIME_FORMAT
+        time_string = arguments[0]
+        attributes[:minutes] = parse_minutes(time_string)
+        if time_string =~ /\+$/
+          attributes[:_start_tracker] = true
+        end
+      else
+        raise MiteCmd::Exception.new "the first argument to 'mite add' must be a time specification"
+      end
+
+      if project = find_or_create_project(arguments[1])
+        attributes[:project_id] = project.id
+      end
+      if @arguments[2] && service = find_or_create_service(arguments[2])
+        attributes[:service_id] = service.id
+      end
+      if note = arguments[3..-1] * " "
+        attributes[:note] = note
+      end
+
+      if !project.id || !service.id || !note
+        raise "missing project/service/note"
+      end
+
+      attributes
+    end
+
+    def current
+      tell Mite::Tracker.current ? Mite::Tracker.current.inspect : "No current entry"
+    end
+
+    def report(arguments)
+      if arguments.length > 1
+        raise MiteCmd::Exception.new "The report subcommand takes exactly one argument with a time specification"
+      elsif arguments.length == 1
+        report_date = arguments[0]
+      end
+
+      if @date
+        if report_date
+          tell "warning: ignoring report argument '#{arguments[0]}' in favor of date parameter '#{@date}'".colorize(:yellow)
+        end
+        report_date = @date
+      end
+
+      report_date ||= "today"
+
+      count = 0
+      total_minutes = 0
+      total_revenue = Mite::TimeEntry.all(:params => {:at => report_date, :user_id => 'current'}).each do |time_entry|
+        total_minutes += time_entry.minutes
+        tell "%3d %s" % [ count, time_entry.inspect ]
+        count += 1
+      end.map(&:revenue).compact.sum
+      tell ("     %s:%.2d" % [total_minutes/60, total_minutes-total_minutes/60*60]).colorize(:lightred) + ", " + ("%.2f $" % (total_revenue/100)).colorize(:lightgreen)
+    end
+
+    def destroy_time_entry(arguments)
+      if arguments.length != 1
+        raise MiteCmd::Exception.new "The delete subcommand takes exactly one argument with a numeric index"
+      end
+      destroy_index = arguments[0].to_i
+      destroy_date = @date || 'today'
+
+      entries = Mite::TimeEntry.all(:params => {:at => destroy_date, :user_id => 'current'})
+      tell "%3d %s" % [ destroy_index, entries[destroy_index].inspect ]
+      if @confirmed
+        entries[destroy_index].destroy
+        tell "the above entry has been permanently removed".colorize(:red)
+      else
+        tell "not destroying the specified entry, add --really to confirm".colorize(:yellow)
+      end
+    end
+
+    def start(arguments)
+      if time_entry = Mite::TimeEntry.first(:params => {:at => 'today'})
+        time_entry.start_tracker
+        tell time_entry.inspect
+      else
+        tell "Oh my dear! I tried hard, but I could'nt find any time entry for today."
+      end
+    end
+
+    def stop(arguments)
+      if current_tracker = (Mite::Tracker.current ? Mite::Tracker.current.stop : nil)
+        tell current_tracker.time_entry.inspect
+      end
+    end
+
+    def find_or_create_by_name(repository, name)
+      object = repository.first(:params => {:name => name})
+      return nil if name =~ TIME_FORMAT
+      object ? object : repository.create(:name => name)
+    end
+
+    def find_or_create_project(name)
+      find_or_create_by_name(Mite::Project, name)
+    end
+
+    def find_or_create_service(name)
+      find_or_create_by_name(Mite::Service, name)
+    end
+
+    def parse_minutes(string)
+      string = string.sub(/\+$/, '')
+
+      if string.blank?
+        0
+      elsif string =~ /^\d+:\d+$/
+        string.split(':').first.to_i*60 + string.split(':').last.to_i
+      elsif string =~ /^\d+(\.\d+)?:?$/
+        (string.to_f*60).to_i
+      end
+    end
+
+    def note(arguments)
+      if time_entry = Mite::TimeEntry.first(:params => {:at => 'today'})
+        time_entry.note = [time_entry.note, *arguments].compact.join(' ')
+        time_entry.save
+        tell time_entry.inspect
+      end
+    end
+
+  end
+end
